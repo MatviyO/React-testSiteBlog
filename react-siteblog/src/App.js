7449ddb305ab8ff4a47693db626d96f648291c78
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-03-11 18:47:35 +0200

    add1

---
+++ react-siteblog/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div>
+
+            <h1>React App</h1>
+            <Button variant="primary"> Button</Button>
+        </div>
+    );
+}
+
+export default App;
